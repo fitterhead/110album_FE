2fedfd94064a986fe995ad8d4895c617f72de7c3
phi <xuanphiart@gmail.com>
2022-12-29 11:35:53 +0700

    new commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import postReducer from "/Users/morita/Desktop/final project/top120albums-fe/top120albums-fe/src/features/post /postSlice.js";
+import postReducer from "../features/post/postSlice.js";
 import contentReducer from "../features/content/contentSlice";
 const rootReducer = { post: postReducer, content: contentReducer };
 const store = configureStore({ reducer: rootReducer });
